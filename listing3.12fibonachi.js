43c46f01e93e7a5ede4af3305d4cefdc64708245
ElenSaprano <104906701+elensaprano@users.noreply.github.com>
2022-05-05 14:44:15 +0700

    Add files via upload

---
+++ listing3.12fibonachi.js
@@ -0,0 +1,11 @@
+function fibs(n){
+  if (n==1||n==2){
+    return 1
+  } else {
+    return fibs(n-1)+fibs(n-2)
+  }
+}
+document.write("<h4>Числа Фибоначчи</h4>")
+for(var k=1;k<=20;k++){
+  document.write(fibs(k)+" ")
+}
